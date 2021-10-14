5e859879a3727b5f68ce9d464ccd2f70a58fda01
Daniela Plascencia <daniela.plascencia@canonical.com>
2021-10-13 21:22:06 -0500

    src/: enables auth resources creation and adds auth_manifests.yaml

--- src/charm.py
+++ src/charm.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
+import os
 import logging
+from pathlib import Path
 
 from ops.main import main
 from ops.pebble import Layer
 from ops.charm import CharmBase
 from ops.model import ActiveStatus, WaitingStatus, MaintenanceStatus
+
+from lightkube import ApiError, Client, codecs
+from lightkube.types import PatchType
 
 logger = logging.getLogger(__name__)
 
@@ -21,8 +26,9 @@ class TrainingOperatorCharm(CharmBase):
         self._name = self.model.app.name
         self._namespace = self.model.name
         self._manager_service = "manager"
+        self._src_dir = Path(__file__).parent
 
-        self.framework.observe(self.on.install, self._on_config_changed)
+        self.framework.observe(self.on.install, self._on_install)
         self.framework.observe(self.on.config_changed, self._on_config_changed)
         self.framework.observe(
             self.on.training_operator_pebble_ready,
@@ -80,10 +86,34 @@ class TrainingOperatorCharm(CharmBase):
         else:
             self.unit.status = WaitingStatus("Waiting for Pebble in workload container")
 
+    def _patch_auth_resources(self) -> None:
+        """Create the Kubernetes auth resources created by Juju.
+
+        Raises:
+            ApiError: if creating any of the auth resources fails.
+        """
+        self.unit.status = MaintenanceStatus("Setting auth configuration")
+        client = Client()
+        context = {"namespace": self._namespace, "app_name": self._name}
+
+        with open(Path(self._src_dir) / "auth_manifests.yaml") as f:
+            for obj in codecs.load_all_yaml(f, context=context):
+                try:
+                    client.create(obj)
+                except ApiError as e:
+                    logging.error(
+                        f"Creating {obj.metadata.name} failed: {e.status.reason}"
+                    )
+                    raise
+        logging.info("Auth resources successfully created.")
 
     def _on_install(self, _):
-        """Placeholder for install event"""
-        pass
+        """Event handler for on-install events."""
+        auth = self._patch_auth_resources()
+
+        # Check resources were applied correctly
+        if auth:
+            self.unit.status = ActiveStatus()
 
 
 if __name__ == "__main__":
