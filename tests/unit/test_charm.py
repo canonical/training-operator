2149ca31dbaee2c42fdf1ee19ab3fa532c8e8432
i-chvets <113444075+i-chvets@users.noreply.github.com>
2023-03-22 14:42:34 -0400

    Kf 1105 gh75 fix crd upgrade (#87)
    Summary of changes:
    - Added remove and upgrade integration test that reproduced the issue.
    - Added upgrade event handler.
    - Modified K8S resources installation to use forced conflict resolution
      in upgrade path.
    - Added error handling and logging to indicate conflict scenario.
    - Modified code to apply and then apply with conflict resoltion as
      suggested in K8S docs.
    - Added remove_without_resources test case. This should complete
      remove/upgrade test suite.
    - Manually merged GenericCharmRuntimeError.
    - Updated unit tests.
    - Updated logging.
    - Updated doc string.
    - Changed log message to indicate what happened. Still having error
      message displayed for clarity.
    - Moved MaintenanceStatus to apply() to make clearer what is going on.
    
    NOTES:
    - `install` event handler needs to be looked at. Applying K8S
    resources in install event handler might create some issues as described
    in other repos/PRs. This was introduced some time ago to speed up
    deployment. Now it is proposed to review/remove it.
    - Use of Chisme update layer should be added as well.
    
    ---------
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Maksim Beliaev <beliaev.m.s@gmail.com>
    Co-authored-by: Daniela Plascencia <daniela.plascencia@canonical.com>

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -51,7 +51,14 @@ def harness() -> Harness:
 class TestCharm:
     """Test class for TrainingOperatorCharm."""
 
-    def test_not_leader(self, harness: Harness):
+    @patch("charm.TrainingOperatorCharm.k8s_resource_handler")
+    @patch("charm.TrainingOperatorCharm.crd_resource_handler")
+    def test_not_leader(
+        self,
+        _: MagicMock,  # k8s_resource_handler
+        ___: MagicMock,  # crd_resource_handler
+        harness: Harness,
+    ):
         """Test not a leader scenario."""
         harness.begin_with_initial_hooks()
         harness.container_pebble_ready("training-operator")
@@ -104,7 +111,7 @@ class TestCharm:
 
     @patch("charm.TrainingOperatorCharm.k8s_resource_handler")
     @patch("charm.TrainingOperatorCharm.crd_resource_handler")
-    def test_deploy_k8s_resources_success(
+    def test_apply_k8s_resources_success(
         self,
         k8s_resource_handler: MagicMock,
         crd_resource_handler: MagicMock,
@@ -112,7 +119,7 @@ class TestCharm:
     ):
         """Test if K8S resource handler is executed as expected."""
         harness.begin()
-        harness.charm._deploy_k8s_resources()
+        harness.charm._apply_k8s_resources()
         crd_resource_handler.apply.assert_called()
         k8s_resource_handler.apply.assert_called()
         assert isinstance(harness.charm.model.unit.status, MaintenanceStatus)
