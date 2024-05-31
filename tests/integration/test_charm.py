2e31dbe205ac62fad966cf9f9db0ecbb69cf4544
Daniela Plascencia <daniela.plascencia@canonical.com>
2024-05-31 12:47:03 +0200

    skip: remove oci-image resource from test

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -19,7 +19,6 @@ from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
 
-METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 APP_NAME = "training-operator"
 CHARM_LOCATION = None
 
@@ -31,11 +30,9 @@ async def test_build_and_deploy(ops_test: OpsTest):
     Assert on the unit status.
     """
     charm_under_test = await ops_test.build_charm(".")
-    image_path = METADATA["resources"]["training-operator-image"]["upstream-source"]
-    resources = {"training-operator-image": image_path}
 
     await ops_test.model.deploy(
-        charm_under_test, resources=resources, application_name=APP_NAME, trust=True
+        charm_under_test, application_name=APP_NAME, trust=True
     )
     await ops_test.model.wait_for_idle(
         apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=60 * 10
@@ -252,12 +249,10 @@ async def test_upgrade(ops_test: OpsTest):
 
     # refresh (upgrade) using charm built in test_build_and_deploy()
     # NOTE: using ops_test.juju() because there is no functionality to refresh in ops_test
-    image_path = METADATA["resources"]["training-operator-image"]["upstream-source"]
     await ops_test.juju(
         "refresh",
         APP_NAME,
         f"--path={CHARM_LOCATION}",
-        f'--resource="training-operator-image={image_path}"',
         "--trust",
     )
     await ops_test.model.wait_for_idle(
