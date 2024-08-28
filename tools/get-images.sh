26cd6203039d090edb74ba746166ffbdb7f20ac1
mvlassis <manos.vlassis@canonical.com>
2024-08-28 15:09:33 +0300

    Update tools/get-images.sh to capture images properly

--- tools/get-images.sh
+++ tools/get-images.sh
@@ -4,6 +4,6 @@
 #
 # dynamic list
 IMAGE_LIST=()
-IMAGE_LIST+=($(find -type f -name metadata.yaml -exec yq '.resources | to_entries | .[] | .value | ."upstream-source"' {} \;))
+IMAGE_LIST+=($(find -type f -name config.yaml -exec yq '.options | with_entries(select(.key | test("-image$"))) | .[].default' {} \;))
 printf "%s\n" "${IMAGE_LIST[@]}"
 
