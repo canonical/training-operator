5fd133a397b3d66c2aae3f557183dc107adde9b1
Dario Faccin <dario.faccin@canonical.com>
2026-01-14 18:08:20 +0100

    fix: increase retries for job status
    Signed-off-by: Dario Faccin <dario.faccin@canonical.com>

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -253,7 +253,7 @@ async def test_create_training_jobs(ops_test: OpsTest, example: str):
     # we have an implementation in lightkube
     @tenacity.retry(
         wait=tenacity.wait_exponential(multiplier=2, min=1, max=30),
-        stop=tenacity.stop_after_attempt(30),
+        stop=tenacity.stop_after_attempt(45),
         reraise=True,
     )
     def assert_job_status_running_success():
